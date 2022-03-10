1cb47d4abb21abe5bc59d0d6ae2b0712b0b663b2
Stefan Kopf <s.kopf@xaldon.com>
2022-03-10 20:52:27 +0100

    Update to latest 0.4 spec: support AAD in signatures

--- spxp-crypto-tools/src/main/java/org/spxp/crypto/tool/SpxpCryptoTool.java
+++ spxp-crypto-tools/src/main/java/org/spxp/crypto/tool/SpxpCryptoTool.java
@@ -22,8 +22,8 @@ import org.spxp.crypto.SpxpConnectKeyPair;
 import org.spxp.crypto.SpxpConnectPublicKey;
 import org.spxp.crypto.SpxpCryptoException;
 import org.spxp.crypto.SpxpCryptoNoSuchKeyException;
-import org.spxp.crypto.SpxpCryptoToolsV03;
-import org.spxp.crypto.SpxpCryptoToolsV03.KeyIdSize;
+import org.spxp.crypto.SpxpCryptoToolsV04;
+import org.spxp.crypto.SpxpCryptoToolsV04.KeyIdSize;
 import org.spxp.crypto.SpxpKeyProvider;
 import org.spxp.crypto.SpxpProfileKeyPair;
 import org.spxp.crypto.SpxpProfilePublicKey;
@@ -92,7 +92,7 @@ public class SpxpCryptoTool {
         System.out.println("      generates a new profile keypair");
         System.out.println("  extractprofilepublic <keyPairFile>");
         System.out.println("      extracts just the public key part from the profile keypair stored in <keyPairFile>");
-        System.out.println("  sign <jsonFileToSign> <keyPairFile>");
+        System.out.println("  sign [-aad <additionallyAuthenticatedData>] <jsonFileToSign> <keyPairFile>");
         System.out.println("      signs the json object in <jsonFileToSign> with the secret key stored in <keyPairFile>");
         System.out.println("  verify <signedJsonFile> <publicKeyFile> [<requiredGrant>[,<requiredGrant>]*]");
         System.out.println("      verifies the signature in <signedJsonFile> with the public key stored in <publicKeyFile>");
@@ -140,9 +140,9 @@ public class SpxpCryptoTool {
             System.out.println("Error: Command 'genprofilekeypair' does not take any options");
             return;
         }
-        SpxpProfileKeyPair keypair = SpxpCryptoToolsV03.generateProfileKeyPair();
+        SpxpProfileKeyPair keypair = SpxpCryptoToolsV04.generateProfileKeyPair();
         try(PrintWriter writer = new PrintWriter(System.out)) {
-            SpxpCryptoToolsV03.getKeypairJWK(keypair).write(writer, 4, 0);
+            SpxpCryptoToolsV04.getKeypairJWK(keypair).write(writer, 4, 0);
         }
     }
     
@@ -152,22 +152,36 @@ public class SpxpCryptoTool {
             return;
         }
         JSONObject keypairJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
-        SpxpProfileKeyPair keypair = SpxpCryptoToolsV03.getProfileKeyPair(keypairJwkObj);
-        JSONObject publicJwk = SpxpCryptoToolsV03.getPublicJWK(keypair.extractProfilePublicKey());
+        SpxpProfileKeyPair keypair = SpxpCryptoToolsV04.getProfileKeyPair(keypairJwkObj);
+        JSONObject publicJwk = SpxpCryptoToolsV04.getPublicJWK(keypair.extractProfilePublicKey());
         try(PrintWriter writer = new PrintWriter(System.out)) {
             publicJwk.write(writer, 4, 0);
         }
     }
     
     public void sign(String[] args) throws Exception {
-        if(args.length != 3) {
+        if(args.length != 3 && args.length != 5) {
             System.out.println("Error: Invalid number of options for command 'sign'");
             return;
         }
-        JSONObject objToSign = new JSONObject(new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
-        JSONObject keypairJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8));
-        SpxpProfileKeyPair keypair = SpxpCryptoToolsV03.getProfileKeyPair(keypairJwkObj);
-        SpxpCryptoToolsV03.signObject(objToSign, keypair);
+        String aad = null;
+        String jsonFileToSign, keyPairFile;
+        if(args.length == 5) {
+            if(!args[1].equals("-aad")) {
+                System.out.println("Error: Invalid number of options for command 'sign'");
+                return;
+            }
+            aad = args[2];
+            jsonFileToSign = args[3];
+            keyPairFile = args[4];
+        } else {
+            jsonFileToSign = args[1];
+            keyPairFile = args[2];
+        }
+        JSONObject objToSign = new JSONObject(new String(Files.readAllBytes(Paths.get(jsonFileToSign)), StandardCharsets.UTF_8));
+        JSONObject keypairJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(keyPairFile)), StandardCharsets.UTF_8));
+        SpxpProfileKeyPair keypair = SpxpCryptoToolsV04.getProfileKeyPair(keypairJwkObj);
+        SpxpCryptoToolsV04.signObject(objToSign, keypair, aad);
         try(PrintWriter writer = new PrintWriter(System.out)) {
             objToSign.write(writer, 4, 0);
         }
@@ -181,8 +195,8 @@ public class SpxpCryptoTool {
         JSONObject signedObject = new JSONObject(new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
         JSONObject publicJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8));
         List<String> requiredGrants = args.length > 3 ? Arrays.asList(args[3].split(",")) : null;
-        SpxpProfilePublicKey publicKey = SpxpCryptoToolsV03.getProfilePublicKey(publicJwkObj);
-        if(SpxpCryptoToolsV03.verifySignature(signedObject, publicKey, requiredGrants)) {
+        SpxpProfilePublicKey publicKey = SpxpCryptoToolsV04.getProfilePublicKey(publicJwkObj);
+        if(SpxpCryptoToolsV04.verifySignature(signedObject, publicKey, null, requiredGrants)) {
             System.out.println("Signature valid.");
             System.exit(0);
         } else {
@@ -211,8 +225,8 @@ public class SpxpCryptoTool {
         }
         JSONObject inputObject = new JSONObject(new String(Files.readAllBytes(Paths.get(jsonFileName)), StandardCharsets.UTF_8));
         try {
-            Set<String> omitMembers = omitSpecial ? SpxpCryptoToolsV03.OMIT_MEMBERS_VERIFY : null;
-            System.out.println(SpxpCryptoToolsV03.canonicalize(inputObject, omitMembers));
+            Set<String> omitMembers = omitSpecial ? SpxpCryptoToolsV04.OMIT_MEMBERS_VERIFY : null;
+            System.out.println(SpxpCryptoToolsV04.canonicalize(inputObject, omitMembers));
         } catch (JSONException | IOException e) {
             throw new SpxpCryptoException("Error canonicalizing object", e);
         }
@@ -223,11 +237,11 @@ public class SpxpCryptoTool {
             System.out.println("Error: Command 'gensymkey' does not take any options");
             return;
         }
-        byte[] key = SpxpCryptoToolsV03.generateSymmetricKey(256);
-        String kid = SpxpCryptoToolsV03.generateRandomKeyId(KeyIdSize.LONG);
+        byte[] key = SpxpCryptoToolsV04.generateSymmetricKey(256);
+        String kid = SpxpCryptoToolsV04.generateRandomKeyId(KeyIdSize.LONG);
         SpxpSymmetricKeySpec keySpec = new SpxpSymmetricKeySpec(kid, key);
         try(PrintWriter writer = new PrintWriter(System.out)) {
-            SpxpCryptoToolsV03.getSymmetricJWK(keySpec).write(writer, 4, 0);
+            SpxpCryptoToolsV04.getSymmetricJWK(keySpec).write(writer, 4, 0);
         }
     }
     
@@ -236,11 +250,11 @@ public class SpxpCryptoTool {
             System.out.println("Error: Command 'gensymkey' does not take any options");
             return;
         }
-        byte[] key = SpxpCryptoToolsV03.generateSymmetricKey(256);
-        String kid = SpxpCryptoToolsV03.generateRandomKeyId(KeyIdSize.LONG)+"."+SpxpCryptoToolsV03.generateRandomKeyId(KeyIdSize.SHORT);
+        byte[] key = SpxpCryptoToolsV04.generateSymmetricKey(256);
+        String kid = SpxpCryptoToolsV04.generateRandomKeyId(KeyIdSize.LONG)+"."+SpxpCryptoToolsV04.generateRandomKeyId(KeyIdSize.SHORT);
         SpxpSymmetricKeySpec keySpec = new SpxpSymmetricKeySpec(kid, key);
         try(PrintWriter writer = new PrintWriter(System.out)) {
-            SpxpCryptoToolsV03.getSymmetricJWK(keySpec).write(writer, 4, 0);
+            SpxpCryptoToolsV04.getSymmetricJWK(keySpec).write(writer, 4, 0);
         }
     }
     
@@ -251,8 +265,8 @@ public class SpxpCryptoTool {
         }
         String payload = (new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
         JSONObject symJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8));
-        SpxpSymmetricKeySpec keySpec = SpxpCryptoToolsV03.getSymmetricKeySpec(symJwkObj);
-        System.out.println(SpxpCryptoToolsV03.encryptSymmetricCompact(payload, keySpec));
+        SpxpSymmetricKeySpec keySpec = SpxpCryptoToolsV04.getSymmetricKeySpec(symJwkObj);
+        System.out.println(SpxpCryptoToolsV04.encryptSymmetricCompact(payload, keySpec));
     }
     
     public void decryptsymcompact(String[] args) throws Exception {
@@ -262,8 +276,8 @@ public class SpxpCryptoTool {
         }
         String ciphertext = (new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
         JSONObject symJwkObj = args.length > 2 ? new JSONObject(new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8)) : null;
-        final SpxpSymmetricKeySpec keySpec = symJwkObj != null ? SpxpCryptoToolsV03.getSymmetricKeySpec(symJwkObj) : null;
-        System.out.println(SpxpCryptoToolsV03.decryptSymmetricCompact(ciphertext, new SpxpKeyProvider() {
+        final SpxpSymmetricKeySpec keySpec = symJwkObj != null ? SpxpCryptoToolsV04.getSymmetricKeySpec(symJwkObj) : null;
+        System.out.println(SpxpCryptoToolsV04.decryptSymmetricCompact(ciphertext, new SpxpKeyProvider() {
             @Override
             public SecretKey getKey(String keyId) throws SpxpCryptoNoSuchKeyException {
                 if(keySpec == null) {
@@ -287,10 +301,10 @@ public class SpxpCryptoTool {
         List<SpxpSymmetricKeySpec> keys = new LinkedList<>();
         for(String s : Arrays.asList(args[2].split(","))) {
             JSONObject symJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(s)), StandardCharsets.UTF_8));
-            SpxpSymmetricKeySpec keySpec = SpxpCryptoToolsV03.getSymmetricKeySpec(symJwkObj);
+            SpxpSymmetricKeySpec keySpec = SpxpCryptoToolsV04.getSymmetricKeySpec(symJwkObj);
             keys.add(keySpec);
         }
-        JSONObject obj = new JSONObject(SpxpCryptoToolsV03.encryptSymmetricJson(payload, keys));
+        JSONObject obj = new JSONObject(SpxpCryptoToolsV04.encryptSymmetricJson(payload, keys));
         try(PrintWriter writer = new PrintWriter(System.out)) {
             obj.write(writer, 4, 0);
         }
@@ -303,8 +317,8 @@ public class SpxpCryptoTool {
         }
         String ciphertext = (new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
         JSONObject symJwkObj = args.length > 2 ? new JSONObject(new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8)) : null;
-        final SpxpSymmetricKeySpec keySpec = symJwkObj != null ? SpxpCryptoToolsV03.getSymmetricKeySpec(symJwkObj) : null;
-        System.out.println(SpxpCryptoToolsV03.decryptSymmetricJson(ciphertext, new SpxpKeyProvider() {
+        final SpxpSymmetricKeySpec keySpec = symJwkObj != null ? SpxpCryptoToolsV04.getSymmetricKeySpec(symJwkObj) : null;
+        System.out.println(SpxpCryptoToolsV04.decryptSymmetricJson(ciphertext, new SpxpKeyProvider() {
             @Override
             public SecretKey getKey(String keyId) throws SpxpCryptoNoSuchKeyException {
                 if(keySpec == null) {
@@ -327,7 +341,7 @@ public class SpxpCryptoTool {
         FileInputStream src = new FileInputStream(new File(args[1]));
         FileOutputStream dest = new FileOutputStream(new File(args[2]));
         String uri = args.length > 3 ? args[3] : null;
-        JSONObject obj = new JSONObject(SpxpCryptoToolsV03.encryptResource(src, dest, uri));
+        JSONObject obj = new JSONObject(SpxpCryptoToolsV04.encryptResource(src, dest, uri));
         try(PrintWriter writer = new PrintWriter(System.out)) {
             obj.write(writer, 4, 0);
         }
@@ -341,7 +355,7 @@ public class SpxpCryptoTool {
         String resJson = new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8);
         try(FileInputStream src = new FileInputStream(new File(args[1]))) {
             try(FileOutputStream dest = new FileOutputStream(new File(args[3]))) {
-                SpxpCryptoToolsV03.decryptResource(src, dest, resJson);
+                SpxpCryptoToolsV04.decryptResource(src, dest, resJson);
             }
         }
     }
@@ -351,9 +365,9 @@ public class SpxpCryptoTool {
             System.out.println("Error: Command 'genconnectkeypair' does not take any options");
             return;
         }
-        SpxpConnectKeyPair keypair = SpxpCryptoToolsV03.generateConnectKeyPair();
+        SpxpConnectKeyPair keypair = SpxpCryptoToolsV04.generateConnectKeyPair();
         try(PrintWriter writer = new PrintWriter(System.out)) {
-            SpxpCryptoToolsV03.getKeypairJWK(keypair).write(writer, 4, 0);
+            SpxpCryptoToolsV04.getKeypairJWK(keypair).write(writer, 4, 0);
         }
     }
     
@@ -363,8 +377,8 @@ public class SpxpCryptoTool {
             return;
         }
         JSONObject keypairJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
-        SpxpConnectKeyPair keypair = SpxpCryptoToolsV03.getConnectKeyPair(keypairJwkObj);
-        JSONObject publicJwk = SpxpCryptoToolsV03.getPublicJWK(keypair.extractConnectPublicKey());
+        SpxpConnectKeyPair keypair = SpxpCryptoToolsV04.getConnectKeyPair(keypairJwkObj);
+        JSONObject publicJwk = SpxpCryptoToolsV04.getPublicJWK(keypair.extractConnectPublicKey());
         try(PrintWriter writer = new PrintWriter(System.out)) {
             publicJwk.write(writer, 4, 0);
         }
@@ -377,8 +391,8 @@ public class SpxpCryptoTool {
         }
         String payload = (new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
         JSONObject asymJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8));
-        SpxpConnectPublicKey publicKey = SpxpCryptoToolsV03.getConnectPublicKey(asymJwkObj);
-        JSONObject obj = new JSONObject(SpxpCryptoToolsV03.encryptAsymmetricJson(payload, publicKey));
+        SpxpConnectPublicKey publicKey = SpxpCryptoToolsV04.getConnectPublicKey(asymJwkObj);
+        JSONObject obj = new JSONObject(SpxpCryptoToolsV04.encryptAsymmetricJson(payload, publicKey));
         try(PrintWriter writer = new PrintWriter(System.out)) {
             obj.write(writer, 4, 0);
         }
@@ -391,8 +405,8 @@ public class SpxpCryptoTool {
         }
         String ciphertext = (new String(Files.readAllBytes(Paths.get(args[1])), StandardCharsets.UTF_8));
         JSONObject asymJwkObj = new JSONObject(new String(Files.readAllBytes(Paths.get(args[2])), StandardCharsets.UTF_8));
-        SpxpConnectKeyPair keyPair = SpxpCryptoToolsV03.getConnectKeyPair(asymJwkObj);
-        System.out.println(SpxpCryptoToolsV03.decryptAsymmetricJson(ciphertext, keyPair));
+        SpxpConnectKeyPair keyPair = SpxpCryptoToolsV04.getConnectKeyPair(asymJwkObj);
+        System.out.println(SpxpCryptoToolsV04.decryptAsymmetricJson(ciphertext, keyPair));
     }
 
 }
